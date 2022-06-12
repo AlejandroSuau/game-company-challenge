a69ac46898da0d7c9fc5b561a4507d7f20f1482b
Alejandro <alejandro.suau@gmail.com>
2022-06-12 17:56:20 +0200

    Added a main example of code usage.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <memory>
 
+// Aditional libraries for test example.
+#include <thread>
+#include <iostream>
+
 #include "Map.h"
 #include "MapWalker.h"
 #include "Heuristic.h"
@@ -24,4 +28,52 @@ bool FindPath(std::pair<int, int> start,
                          std::make_unique<Manhattan>());
     
     return map_walker.FindPath();
+}
+
+// The original code should be implemented a main method.
+// This was implemented just for show the multi-thread example usage.
+// The code bellow isn't part of the test.
+
+void PrintPath(std::pair<int, int> map_dimensions, std::vector<int> out_path) {
+    std::cout << std::endl;
+    
+    int i = 0;
+    for (int row = 0; row < map_dimensions.second; ++row) {
+        for (int column = 0; column < map_dimensions.first; ++column) {
+            const bool is_path_solution = std::find(
+                out_path.begin(), out_path.end(), i) != out_path.end();
+            std::cout << (is_path_solution ? "x" : "-");
+            ++i;
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    std::pair<int, int> map_dimensions = {4, 3};
+    std::vector<int> map_locations = {
+        1, 1, 1, 1,
+        0, 1, 0, 1,
+        1, 1, 1, 1
+    };
+    std::pair<int, int> start_unit1 = {0, 0};
+    std::pair<int, int> target_unit1 = {1, 2};
+    std::vector<int> out_path_unit1 = {};
+    
+    std::pair<int, int> start_unit2 = {1, 1};
+    std::pair<int, int> target_unit2 = {3, 1};
+    std::vector<int> out_path_unit2 = {};
+        
+    std::thread t1(FindPath, start_unit1, target_unit1, std::ref(map_locations),
+                   map_dimensions, std::ref(out_path_unit1));
+    
+    std::thread t2(FindPath, start_unit2, target_unit2, std::ref(map_locations),
+                   map_dimensions, std::ref(out_path_unit2));
+    
+    t1.join();
+    PrintPath(map_dimensions, out_path_unit1);
+    t2.join();
+    PrintPath(map_dimensions, out_path_unit2);
+    
+    return 0;
 }
